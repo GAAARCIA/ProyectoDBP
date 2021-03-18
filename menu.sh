d520efd91c8cafdd666f7f862824168d4985907f
Daniel <a329694@uach.mx>
2021-03-18 01:54:20 -0700

    SE ARREGLARON BUGS RELACIONADOS CON LOS PARAMETROS Y SE AÑADIÓ LA VARIABLE nombre_seccion.

--- menu.sh
+++ menu.sh
@@ -2,7 +2,6 @@
 #menu agile
 function _menu_a(){
   clear
-  echo ""
   echo "Bienvenido a la guía rápida de Agile, para continuar seleccione un tema:"
   echo "1. SCRUM"
   echo "2. X.P."
@@ -22,154 +21,165 @@ function _menu_t(){
 }
 #menu informacion
 function _menu_i(){
-echo "1. Agregar información"
-echo "2. Buscar"
-echo "3. Eliminar información"
-echo "4. Leer base de información"
+  nombre=($nombre_seccion)
+  clear
+  echo "Usted esta en la sección $nombre, seleccione la opción que desea utilizar."
+  echo "1. Agregar información"
+  echo "2. Buscar"
+  echo "3. Eliminar información"
+  echo "4. Leer base de información"
 }
-
-
-
-if [ $1 == -a ];then
-	_menu_a
-	read -p "Seleccione la opción que desea utilizar: " opcion1
-		case $opcion1 in
-			1)
-				_menu_i
-				read -p "Seleccione la opción que desea utilizar: " op1
-					case $op1 in
-						1)
-							#Agrega información al archivo SCRUM.inf
-								;;
-						2)
-							#Buscar información en el archivo SCRUM.inf
-								;;
-						3)
-							#Elimina información del archivo SCRUM.inf
-								;;
-						4)
-							#Lee información del archivo SCRUM.inf
-								;;
-						esac
-					;;
-			2)
-				echo " X.P "
-				_menu_i
-				read -p "Seleccione la opción que desea utilizar: " op2
-                                        case $op2 in
-                                                1)
-                                                        #Agrega información al archivo XP.inf
-								;;
-                                                2)
-                                                        #Buscar información en el archivo XP.inf
-								;;
-                                                3)
-                                                        #Elimina información del archivo XP.inf
-								;;
-                                                4)
-                                                        #Lee información del archivo XP.inf
-                                               			 ;;
-						esac
-					;;
-			3)
-				echo " Kanban "
-				_menu_i
-				read -p "Seleccione la opción que desea utilizar: " op3
-                                        case $op3 in
-                                                1)
-                                                        #Agrega información al archivo Kanban.inf
-								;;
-                                                2)
-                                                        #Buscar información en el archivo Kanban.inf
-								;;
-                                                3)
-                                                        #Elimina información del archivo Kanban.inf
-								;;
-                                                4)
-                                                        #Lee información del archivo Kanban.inf
-                                                		;;
-						esac
-					;;
-			4)
-				echo " Crystal "
-				_menu_i
-				read -p "Seleccione la opción que desea utilizar: " op4
-                                        case $op4 in
-                                                1)
-                                                        #Agrega información al archivo Crystal.inf
-								;;
-                                                2)
-                                                        #Buscar información en el archivo Crystal.inf
-								;;
-                                                3)
-                                                        #Elimina información del archivo Crystal.inf
-								;;
-                                                4)
-                                                        #Lee información del archivo Crystal.inf
-                                                		;;
-						esac
-					;;
-			esac
-else
-	_menu_t
-	read -p "Seleccione la opción que desea utilizar: " opcion2
-		case $opcion2 in
-			1)
-				echo " Cascada "
-				_menu_i
-				read -p "Seleccione la opción que desea utilizar: " opc1
-                                        case $opc1 in
-                                                1)
-                                                        #Agrega información al archivo Cascada.inf
-								;;
-                                                2)
-                                                        #Buscar información en el archivo Cascada.inf
-								;;
-                                                3)
-                                                        #Elimina información del archivo Cascada.inf
-								;;
-                                                4)
-                                                        #Lee información del archivo Cascada.inf
-                                                		;;
-						esac
-					;;
-			2)
-				echo " Espiral "
-				_menu_i
-				read -p "Seleccione la opción que desea utilizar: " opc2
-                                        case $opc2 in
-                                                1)
-                                                        #Agrega información al archivo Espiral.inf
-								;;
-                                                2)
-                                                        #Buscar información en el archivo Espiral.inf
-								;;
-                                                3)
-                                                        #Elimina información del archivo Espiral.inf
-								;;
-                                                4)
-                                                        #Lee información del archivo Espiral.inf
-                                                		;;
-						esac
-					;;
-			3)
-				echo " Modelo V "
-				_menu_i
-				read -p "Seleccione la opción que desea utilizar: " opc3
-                                        case $opc3 in
-                                                1)
-                                                        #Agrega información al archivo ModeloV.inf
-								;;
-                                                2)
-                                                        #Buscar información en el archivo ModeloV.inf
-								;;
-                                                3)
-                                                        #Elimina información del archivo ModeloV.inf
-								;;
-                                                4)
-                                                        #Lee información del archivo ModeloV.inf
-                                                		;;
-						esac
-					;;
-			esac
-fi
+nombre_seccion=""
+if [ $1 == ""]; then
+  echo "Parametro inesperado. Por favor vuelva a ejecutar y seleccione un parametro."
+  echo "-a"
+  echo "-t"
+elif [ $1 == -a ];then
+  	_menu_a
+  	read -p "Seleccione la opción que desea utilizar: " opcion1
+  	case $opcion1 in
+      1)
+        nombre_seccion="SCRUM"
+        _menu_i
+  		  read op1
+  					case $op1 in
+  						1)
+  							#Agrega información al archivo SCRUM.inf
+  								;;
+  						2)
+  							#Buscar información en el archivo SCRUM.inf
+  								;;
+  						3)
+  							#Elimina información del archivo SCRUM.inf
+  								;;
+  						4)
+  							#Lee información del archivo SCRUM.inf
+  								;;
+  						esac
+  					;;
+        2)
+          nombre_seccion="X.P."
+  				_menu_i
+  				read op2
+              case $op2 in
+                1)
+                  #Agrega información al archivo XP.inf
+  								;;
+                2)
+                  #Buscar información en el archivo XP.inf
+  								;;
+                3)
+                  #Elimina información del archivo XP.inf
+  								;;
+                4)
+                  #Lee información del archivo XP.inf
+                  ;;
+  						esac
+  					;;
+  			3)
+          nombre_seccion="Kanban"
+  				echo " Kanban "
+  				_menu_i
+  				read op3
+          case $op3 in
+            1)
+              #Agrega información al archivo Kanban.inf
+  								;;
+            2)
+              #Buscar información en el archivo Kanban.inf
+  								;;
+            3)
+              #Elimina información del archivo Kanban.inf
+  								;;
+            4)
+              #Lee información del archivo Kanban.inf
+                  ;;
+  						esac
+  					;;
+  			4)
+  			  nombre_seccion="Crystal"
+  				_menu_i
+  				read op4
+          case $op4 in
+            1)
+              #Agrega información al archivo Crystal.inf
+  								;;
+            2)
+              #Buscar información en el archivo Crystal.inf
+  								;;
+            3)
+              #Elimina información del archivo Crystal.inf
+  								;;
+            4)
+              #Lee información del archivo Crystal.inf
+                  ;;
+  						esac
+  					;;
+  			esac
+  elif [ $1 == -t ];then
+  	_menu_t
+  	read -p "Seleccione la opción que desea utilizar: " opcion2
+  		case $opcion2 in
+  			1)
+  				nombre_seccion="Cascada"
+  				_menu_i
+  				read -p "Seleccione la opción que desea utilizar: " opc1
+          case $opc1 in
+            1)
+              #Agrega información al archivo Cascada.inf
+  								;;
+            2)
+              #Buscar información en el archivo Cascada.inf
+  								;;
+            3)
+              #Elimina información del archivo Cascada.inf
+  								;;
+            4)
+              #Lee información del archivo Cascada.inf
+                  ;;
+  						esac
+  					;;
+  			2)
+  				nombre_seccion="Espiral"
+  				_menu_i
+  				read -p "Seleccione la opción que desea utilizar: " opc2
+            case $opc2 in
+              1)
+                #Agrega información al archivo Espiral.inf
+  								;;
+              2)
+                #Buscar información en el archivo Espiral.inf
+  								;;
+              3)
+                #Elimina información del archivo Espiral.inf
+  								;;
+              4)
+                #Lee información del archivo Espiral.inf
+                  ;;
+  						esac
+  					;;
+  			3)
+  				nombre_seccion="ModeloV"
+  				_menu_i
+  				read -p "Seleccione la opción que desea utilizar: " opc3
+            case $opc3 in
+              1)
+                #Agrega información al archivo ModeloV.inf
+  								;;
+              2)
+                #Buscar información en el archivo ModeloV.inf
+  								;;
+              3)
+                #Elimina información del archivo ModeloV.inf
+  								;;
+              4)
+                #Lee información del archivo ModeloV.inf
+                  ;;
+  						esac
+  					;;
+  			esac
+  else
+    echo "Parametro inesperado. Por favor vuelva a ejecutar y seleccione un parametro."
+    echo "-a"
+    echo "-t"
+  fi
